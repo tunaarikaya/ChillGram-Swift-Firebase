db8a0d3656ceb7f41df39a8551b2326dd2527bd3
Tuna Arikaya <155156717+tunaarikaya@users.noreply.github.com>
2024-11-24 14:25:20 +0300

    Update SceneDelegate.swift

--- InstaFirebase/InstaFirebase/SceneDelegate.swift
+++ InstaFirebase/InstaFirebase/SceneDelegate.swift
@@ -12,7 +12,7 @@ import FirebaseAuth
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    //startss
+    //startssss
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
